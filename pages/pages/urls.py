b3b64c0118be3395f81628e45ff74b6599eb9e8c
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-09-16 16:11:59 +0300

    start using templates

---
+++ pages/pages/urls.py
@@ -0,0 +1,9 @@
+# this is a new file created to add urls to this app
+from django.urls import path
+from .views import HomePageView, AboutPageView
+
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),      # New
+]
