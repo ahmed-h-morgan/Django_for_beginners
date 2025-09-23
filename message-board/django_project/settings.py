c5cc44ce29fed733aaa6a2b5e62f1cb290c96fe5
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-09-23 14:11:12 +0300

    complete messaage-board app by adding new tests using TestCase and setUpTestData (class method) , install Gunicorn and create requirements file

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^p37#+7$7uw7n++cwb(po1293j&vphpt81%gxa!_==_a#nmw!l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
