3ec3706330cbbed4bde2c06318f947094fd62447
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-10-15 08:21:50 +0000

    complete updateing posts

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
-from django.views.generic.edit import CreateView      # NEW
+from django.views.generic.edit import CreateView, UpdateView      # NEW
 
 # Create your views here.
 class BlogListView(ListView):
@@ -17,3 +17,7 @@ class BlogCreateView(CreateView):
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
 
+class BlogUpdateView(UpdateView): # new 
+    model = Post 
+    template_name = "post_edit.html" 
+    fields = ["title", "body"]
