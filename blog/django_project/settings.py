ec8cb7d6882cad9dfc02e78700842ea94425d7d1
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-10-19 10:52:41 +0000

    create login_page + add LOGIN_REDIRECT_URL to redirect to page after successful login

--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]         # new
 
+LOGIN_REDIRECT_URL = "home" # new
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
