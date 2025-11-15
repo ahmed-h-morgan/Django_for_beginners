7e13588860035f0aad7b91bc4d12cf05fe80b63b
ahmed-h-morgan <ahmedmorgan210@gmail.com>
2025-11-15 18:24:12 +0200

    update admin page and article pages to display related commnets

--- news/articles/admin.py
+++ news/articles/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from .models import Article     # NEW
+from .models import Article, Comment     # NEW
 
 # Register your models here.
 
-admin.site.register(Article)      # NEW - register articles to admin
+class CommentInline(admin.StackedInline): # new - use StackedInline to display the mouuels relations in admin page
+    model = Comment
+    extra = 0 # new  - update the value of this attribute from 3 (the default value) to 0
+
+
+# class CommentInline(admin.TabularInline): # new  - use TabularInline to display the mouuels relations in admin page
+#     model = Comment
+
+class ArticleAdmin(admin.ModelAdmin): # new 
+    inlines = [ CommentInline, ]
+
+
+admin.site.register(Article, ArticleAdmin)      # NEW - register articles to admin
+admin.site.register(Comment) # new - register comment - model to admin to make it visible into the admin page
