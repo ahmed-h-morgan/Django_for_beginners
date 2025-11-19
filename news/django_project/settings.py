8587c1bb438eb24bea3ee5df8894a80d1822ab36
ahmed-h-morgan <ahmedmorgan210@gmail.com>
2025-11-19 03:35:00 +0200

    start to use env variables

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from environs import Env     # new - to be able to use Environment Variables
+
+
+env = Env() # new
+env.read_env() # new
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
